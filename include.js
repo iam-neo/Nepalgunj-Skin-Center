897160a8cf2dbdf7c2c5fce06514d60f5dd72d9e
iam-neo <nirmalrokamagar54@gmail.com>
2025-12-07 17:17:32 +0545

    New way to deploy

---
+++ include.js
@@ -0,0 +1,14 @@
+function includeHTML(id, file) {
+    fetch(file)
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById(id).innerHTML = data;
+      })
+      .catch(err => {
+        console.error("Error loading file:", file, err);
+      });
+  }
+  
+  includeHTML("header", "/header.html");
+  includeHTML("footer", "/footer.html");
+  
